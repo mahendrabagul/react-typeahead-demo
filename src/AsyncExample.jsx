0c957baddaa5566950a1baaa54cb777231c23819
Mahendra Bagul <mahendra@infracloud.com>
2019-11-20 17:21:41 +0530

    Async is now ready to use

--- src/AsyncExample.jsx
+++ src/AsyncExample.jsx
@@ -1,61 +1,43 @@
 import React, {Fragment} from 'react';
-import {FormGroup} from 'react-bootstrap';
 import {AsyncTypeahead} from 'react-bootstrap-typeahead';
-import Control from './Control';
 import GithubMenuItem from './GithubMenuItem';
 import makeAndHandleRequest from './makeAndHandleRequest';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
-class AsyncExample extends React.Component {
-    state = {
-        allowNew: false,
-        isLoading: false,
-        multiple: false,
-        options: [],
-    };
+const getInitialState = () => ({
+    isLoading: false,
+    options: [],
+    selected:[],
+});
 
+class AsyncExample extends React.Component {
+    state = getInitialState();
     render() {
+        const {selected} = this.state;
+
+        if (selected.length) {
+            console.log(JSON.stringify(selected));
+        }
         return (
             <Fragment>
                 <AsyncTypeahead
+                    id="asyncTypeahead"
                     {...this.state}
                     labelKey="login"
                     minLength={3}
+                    multiple
+                    selected={selected}
+                    align="justify"
                     onSearch={this._handleSearch}
+                    onChange={(s) => this.setState({selected: s})}
                     placeholder="Search for a Github user..."
                     renderMenuItemChildren={(option, props) => (
                         <GithubMenuItem key={option.id} user={option} />
                     )}
                 />
-                <FormGroup>
-                    {this._renderCheckboxes()}
-                </FormGroup>
             </Fragment>
         );
     }
-
-    _renderCheckboxes() {
-        const checkboxes = [
-            {label: 'Multi-Select', name: 'multiple'},
-            {label: 'Allow custom selections', name: 'allowNew'},
-        ];
-
-        return checkboxes.map(({label, name}) => (
-            <Control
-                checked={this.state[name]}
-                key={name}
-                name={name}
-                onChange={this._handleChange}
-                type="checkbox">
-                {label}
-            </Control>
-        ));
-    }
-
-    _handleChange = (e) => {
-        const {checked, name} = e.target;
-        this.setState({[name]: checked});
-    };
 
     _handleSearch = (query) => {
         this.setState({isLoading: true});
